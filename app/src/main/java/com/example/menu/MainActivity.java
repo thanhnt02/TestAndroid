48709ce2e2d7907ccaa92899dd4cdfe8fed82fcf
Nguyễn Trí Thành <nguyentrithanh2409@gmail.com>
2023-12-01 10:13:05 +0700

    Commit của Nguyệt

--- app/src/main/java/com/example/menu/MainActivity.java
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.menu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_demo, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
